03beabff60c6c12d1216dd31e0cd8d1b20c280ad
ramon541 <ramondiasmonteiro@gmail.com>
2023-05-27 13:59:30 -0300

    I added a function that when the plus button is press, add 1 to the respective type of Robotrom part and also add to the status of Robotron

---
+++ js/main.js
@@ -0,0 +1,78 @@
+// const robotron = document.querySelector("#robotron");
+
+// robotron.addEventListener("click", () => {
+//   console.log("Oi");
+// });
+const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+
+const pecas = {
+  bracos: {
+    forca: 29,
+    poder: 35,
+    energia: -21,
+    velocidade: -5,
+  },
+
+  blindagem: {
+    forca: 41,
+    poder: 20,
+    energia: 0,
+    velocidade: -20,
+  },
+  nucleos: {
+    forca: 0,
+    poder: 7,
+    energia: 48,
+    velocidade: -24,
+  },
+  pernas: {
+    forca: 27,
+    poder: 21,
+    energia: -32,
+    velocidade: 42,
+  },
+  foguetes: {
+    forca: 0,
+    poder: 28,
+    energia: 0,
+    velocidade: -2,
+  },
+};
+
+controle.forEach((elemento) => {
+  elemento.addEventListener("click", (evento) => {
+    manipulaDados(evento.target.textContent, evento.target.parentNode);
+    atualizaEstatisticas(evento.target.textContent, evento.target.dataset.peca);
+  });
+});
+
+function manipulaDados(operacao, controle) {
+  const peca = controle.querySelector("[data-contador]");
+
+  if (operacao === "-") {
+    if (peca.value > 0) {
+      peca.value = parseInt(peca.value) - 1;
+    }
+  } else {
+    peca.value = parseInt(peca.value) + 1;
+  }
+}
+
+function atualizaEstatisticas(operacao, peca) {
+  const valor = document.querySelectorAll("[data-contador]");
+  console.log(valor.value);
+  if (operacao === "-") {
+    estatisticas.forEach((elemento) => {
+      elemento.textContent =
+        parseInt(elemento.textContent) -
+        pecas[peca][elemento.dataset.estatistica];
+    });
+  } else {
+    estatisticas.forEach((elemento) => {
+      elemento.textContent =
+        parseInt(elemento.textContent) +
+        pecas[peca][elemento.dataset.estatistica];
+    });
+  }
+}
